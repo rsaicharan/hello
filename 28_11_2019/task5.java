cea996e226df45f01076ec5990fe60c3576de9f2
yendalachinnaswetha <swethayendala456@gmail.com>
2019-11-30 17:26:14 +0530

    hello

---
+++ 28_11_2019/task5.java
@@ -0,0 +1,14 @@
+class Demo
+{
+    public static void main(String[] args)
+    {
+        for(char i='A';i<='E';i++)
+        {
+            for(char j='A';j<='E';j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
